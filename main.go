56f9bff8ca3928fbb8f4fa0c8196c5bbf0f8c8f4
Maximilian Prusch <maximilian.prusch@outlook.com>
2023-05-13 21:35:32 +0200

    Add helpers for better debugging

--- main.go
+++ main.go
@@ -3,30 +3,66 @@ package main
 import (
 	"fmt"
 	"github.com/kruspe/nvram/nvram"
+	"strconv"
 )
+
+type helper struct {
+	prefix string
+	value  string
+	nvram  *nvram.Nvram
+
+	writeFailure  []int
+	deleteFailure []int
+}
 
 func main() {
 	n := nvram.New()
 	defer n.Teardown()
-
-	err := n.Set("test", "test")
-	if err != nil {
-		panic(err)
+	value := ""
+	for i := 0; i < 1; i++ {
+		value += "a"
 	}
-
-	value, err := n.Get("test")
-	if err != nil {
-		panic(err)
+	h := &helper{
+		prefix: "test",
+		value:  value,
+		nvram:  n,
 	}
-	fmt.Println(value)
+	defer func() {
+		if len(h.writeFailure) > 0 {
+			fmt.Printf("write failures: %v\n", h.writeFailure)
+		}
+		if len(h.deleteFailure) > 0 {
+			fmt.Printf("delete failures: %v\n", h.deleteFailure)
+		}
+	}()
+}
 
-	err = n.Delete("test")
-	if err != nil {
-		panic(err)
+func (h *helper) write(times int) {
+	for i := 0; i < times; i++ {
+		err := h.nvram.Set(fmt.Sprintf("%s%d", h.prefix, i), h.value)
+		if err != nil {
+			h.writeFailure = append(h.writeFailure, i)
+		}
 	}
+}
 
-	value, err = n.Get("test")
-	if err != nil {
-		fmt.Println(err)
+func (h *helper) get(times int) {
+	for i := 0; i < times; i++ {
+		key := h.prefix + strconv.Itoa(i)
+		value, err := h.nvram.Get(key)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%s: %s\n", key, value)
+	}
+}
+
+func (h *helper) delete(times int) {
+	for i := 0; i < times; i++ {
+		err := h.nvram.Delete(fmt.Sprintf("%s%d", h.prefix, i))
+		if err != nil {
+			h.deleteFailure = append(h.deleteFailure, i)
+		}
 	}
 }
