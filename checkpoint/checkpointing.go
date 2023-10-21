e85c0c477ad6447c41195694bcd07d578a044e6a
Maximilian Prusch <maximilian.prusch@outlook.com>
2023-10-21 20:54:33 +0200

    Add big matrix multiplication with gob comparisson

--- checkpoint/checkpointing.go
+++ checkpoint/checkpointing.go
@@ -1,14 +1,17 @@
 package checkpoint
 
 import (
+	"encoding/gob"
 	"encoding/json"
 	"github.com/kruspe/nvram/nvram"
+	"io"
 	"os"
 )
 
 type Checkpoint struct {
-	nvram *nvram.Nvram
-	file  *os.File
+	nvram    *nvram.Nvram
+	jsonFile *os.File
+	gobFile  *os.File
 }
 
 type Data struct {
@@ -17,13 +20,18 @@ type Data struct {
 }
 
 func NewCheckpoint(nvram *nvram.Nvram) (*Checkpoint, error) {
-	file, err := os.Create("out/checkpoint.json")
+	jsonFile, err := os.Create("out/checkpoint.json")
+	if err != nil {
+		return nil, err
+	}
+	gobFile, err := os.Create("out/checkpoint.gob")
 	if err != nil {
 		return nil, err
 	}
 	return &Checkpoint{
-		nvram: nvram,
-		file:  file,
+		nvram:    nvram,
+		jsonFile: jsonFile,
+		gobFile:  gobFile,
 	}, nil
 }
 
@@ -36,7 +44,23 @@ func (c *Checkpoint) Regular(data []Data) error {
 	if err != nil {
 		return err
 	}
-	_, err = c.file.WriteAt(marshal, 0)
+	_, err = c.jsonFile.WriteAt(marshal, 0)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Checkpoint) Gob(data []Data) error {
+	s := make(map[string]string)
+	for _, d := range data {
+		s[d.Key] = d.Value
+	}
+	err := gob.NewEncoder(c.gobFile).Encode(s)
+	if err != nil {
+		return err
+	}
+	_, err = c.gobFile.Seek(0, io.SeekStart)
 	if err != nil {
 		return err
 	}
--- problem/matrix.go
+++ problem/matrix.go
@@ -62,6 +62,39 @@ func (p *Problem) MultiplyRegularCheckpoints(a [][]int, b [][]int) ([][]int, err
 	return result, nil
 }
 
+func (p *Problem) MultiplyGobCheckpoints(a [][]int, b [][]int) ([][]int, error) {
+	if len(a[0]) != len(b) {
+		return nil, errors.New("rows of a must be equal to columns of b")
+	}
+
+	result := make([][]int, len(a))
+	for i := range result {
+		result[i] = make([]int, len(b[0]))
+	}
+	for i := range result {
+		for j := range b[0] {
+			var r = 0
+			for x := range b {
+				r += a[i][x] * b[x][j]
+			}
+			result[i][j] = r
+			currentStep := i*len(b) + j
+			data := createCurrentData(result, currentStep)
+			err := p.checkpoint.Gob([]checkpoint.Data{
+				{
+					Key:   "currentResult",
+					Value: data,
+				},
+			})
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+		}
+
+	}
+	return result, nil
+}
+
 func (p *Problem) MultiplyNvramCheckpoints(a [][]int, b [][]int) ([][]int, error) {
 	if len(a[0]) != len(b) {
 		return nil, errors.New("rows of a must be equal to columns of b")
