88f29be42d9fddc2ef6a9d73a07414cec7f45a99
Maximilian Prusch <maximilian.prusch@outlook.com>
2023-12-13 18:31:51 +0100

    Write checkpoint when a specific size was hit

--- main.go
+++ main.go
@@ -1,34 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"github.com/kruspe/nvram/benchmark"
 	"github.com/kruspe/nvram/nvram"
-	"github.com/kruspe/nvram/tester"
 )
 
 func main() {
 	n := nvram.NewNvram()
 	defer n.Teardown()
 
-	keys, err := tester.CheckSize(n, 1000)
-	if err != nil {
-		panic(err)
-	}
-	for _, key := range keys {
-		err := n.Delete(key)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
+	//keys, err := tester.CheckSize(n, 1000)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//for _, key := range keys {
+	//	err := n.Delete(key)
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//}
 
 	//err := benchmark.NewBenchmark(n)
 	//if err != nil {
 	//	panic(err)
 	//}
 
-	//err := benchmark.Multiply(n)
-	//if err != nil {
-	//	panic(err)
-	//}
-
+	err := benchmark.Multiply(n)
+	if err != nil {
+		panic(err)
+	}
 }
--- problem/matrix.go
+++ problem/matrix.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/kruspe/nvram/checkpoint"
+	"strconv"
 )
 
 func (p *Problem) Multiply(a [][]int, b [][]int) ([][]int, error) {
@@ -47,15 +48,18 @@ func (p *Problem) MultiplyGobCheckpoints(a [][]int, b [][]int) ([][]int, error) 
 			data += fmt.Sprintf("%d,", r)
 		}
 
-		err := p.checkpoint.Gob.Rewrite([]checkpoint.Data{
-			{
-				Key:   "currentResult",
-				Value: data[:len(data)-1],
-			},
-		})
-		if err != nil {
-			fmt.Println(err.Error())
-			return nil, err
+		if len(data) > 1024*50 {
+			err := p.checkpoint.Gob.Rewrite([]checkpoint.Data{
+				{
+					Key:   "currentResult",
+					Value: data[:len(data)-1],
+				},
+			})
+			if err != nil {
+				fmt.Println(err.Error())
+				return nil, err
+			}
+			data = ""
 		}
 	}
 	return result, nil
@@ -66,11 +70,13 @@ func (p *Problem) MultiplyNvramCheckpoints(a [][]int, b [][]int) ([][]int, error
 		return nil, errors.New("rows of a must be equal to columns of b")
 	}
 
+	checkPointCounter := 0
 	data := ""
 	result := make([][]int, len(a))
 	for i := range result {
 		result[i] = make([]int, len(b[0]))
 	}
+	var lastField string
 	for i := range result {
 		for j := range b[0] {
 			var r = 0
@@ -78,17 +84,26 @@ func (p *Problem) MultiplyNvramCheckpoints(a [][]int, b [][]int) ([][]int, error
 				r += a[i][x] * b[x][j]
 			}
 			result[i][j] = r
+			lastField = fmt.Sprintf("%d,%d", i, j)
 			data += fmt.Sprintf("%d,", r)
 		}
-		err := p.checkpoint.Nvram.Write([]checkpoint.Data{
-			{
-				Key:   "currentResult",
-				Value: data[:len(data)-1],
-			},
-		})
-		if err != nil {
-			fmt.Println(err.Error())
-			return nil, err
+		if len(data) > 1024*50 {
+			err := p.checkpoint.Nvram.Write([]checkpoint.Data{
+				{
+					Key:   "result" + fmt.Sprintf("000000%d", checkPointCounter)[len(strconv.Itoa(checkPointCounter)):],
+					Value: data[:len(data)-1],
+				},
+				{
+					Key:   "lastField",
+					Value: lastField,
+				},
+			})
+			if err != nil {
+				fmt.Println(err.Error())
+				return nil, err
+			}
+			data = ""
+			checkPointCounter++
 		}
 	}
 	return result, nil
